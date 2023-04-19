59294998a57c6b81fce05dcdf07193c82935d3ab
liahra <chrira17@uia.no>
2023-04-19 21:14:01 +0200

    Test OK

--- mycrypt/mycrypt.go
+++ mycrypt/mycrypt.go
@@ -1,11 +1,19 @@
 package mycrypt
 
-var ALF_SEM03 []rune = []rune("abcdefghijklmnopqrstuvwxyzæøå0123456789.,:; ")
+import "fmt"
+
+// Oppdaterer alfabetet til å inkludere store bokstaver.
+var ALF_SEM03 []rune = []rune("abcdefghijklmnopqrstuvwxyzæøåABCDEFGHIJKLMNOPQRSTUVWXYZÆØÅ0123456789.,:; ")
 
 func Krypter(melding []rune, alphabet []rune, chiffer int) []rune {
 	kryptertMelding := make([]rune, len(melding))
 	for i := 0; i < len(melding); i++ {
 		indeks := sokIAlfabetet(melding[i], alphabet)
+		// Legger til en test her som skriver ut om den ikke finner et symbol, som betyr at alfabetet vårt er ufullstendig.
+		if indeks == -1 {
+			fmt.Println("Symbol not found")
+			break
+		}
 		if indeks+chiffer >= len(alphabet) {
 			kryptertMelding[i] = alphabet[indeks+chiffer-len(alphabet)]
 		} else {
@@ -19,7 +27,6 @@ func sokIAlfabetet(symbol rune, alfabet []rune) int {
 	for i := 0; i < len(alfabet); i++ {
 		if symbol == alfabet[i] {
 			return i
-			break
 		}
 	}
 	return -1
--- mycrypt/mycrypt_test.go
+++ mycrypt/mycrypt_test.go
@@ -6,19 +6,19 @@ import (
 )
 
 // Testene forutsetter et alfabet, som er definert i mycrypt.go som ALF_SEM03
-
 func TestKrypter(t *testing.T) {
 
 	type test struct {
 		inputMessage []rune
-                chiffer int
-		want  []rune
+		chiffer      int
+		want         []rune
 	}
 	tests := []test{
 		{inputMessage: []rune("w"), chiffer: 4, want: []rune("æ")},
 		{inputMessage: []rune("0"), chiffer: 4, want: []rune("4")},
-		{inputMessage: []rune("Kjevik;SN39040;18.03.2022 01:50;6"), chiffer: 4, want: []rune("dnizmocdd7;484c5: 47 6466d45b94c.")},
-                {inputMessage: []rune("dnizmocdd7;484c5: 47 6466d45b94c."), chiffer: len(ALF_SEM03) - 4, want: []rune("Kjevik;SN39040;18.03.2022 01:50;6")},
+		// Oppdaterer her sånn at det passer med det oppdaterte alfabetet.
+		{inputMessage: []rune("Kjevik;SN39040;18.03.2022 01:50;6"), chiffer: 4, want: []rune("OnizmocWR7;484c5: 47 6466d45b94c.")},
+		{inputMessage: []rune("OnizmocWR7;484c5: 47 6466d45b94c."), chiffer: len(ALF_SEM03) - 4, want: []rune("Kjevik;SN39040;18.03.2022 01:50;6")},
 	}
 
 	for _, tc := range tests {
@@ -47,3 +47,5 @@ func TestSokIAlfabetet(t *testing.T) {
 		}
 	}
 }
+
+
