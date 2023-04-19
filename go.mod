2d702164f94af9e3ab29d10dbf98257e0b2f22a0
liahra <chrira17@uia.no>
2023-04-19 21:15:26 +0200

    Resten OK

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/uia-worker/is105sem03
+module github.com/liahra/is105sem03
 
-go 1.19
+go 1.20
--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"github.com/uia-worker/is105sem03/mycrypt"
+	"github.com/liahra/is105sem03/mycrypt"
 )
 
 func main() {
